406be2eb5edc42911f9ecc7a5c729d6efe3fc027
Félix Saparelli <felix@passcod.name>
2024-01-23 09:49:19 +1300

    draft: Sketch how to cleanup

--- src/actions/context.rs
+++ src/actions/context.rs
@@ -1,14 +1,17 @@
-use std::io::{stderr, Write};
+use std::{io::{stderr, Write}, sync::{Arc, RwLock}, mem::swap, ops::DerefMut};
 
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use tracing::Metadata;
 use tracing_subscriber::fmt::MakeWriter;
+
+use super::upload::UploadId;
 
 #[derive(Clone, Debug)]
 pub struct Context<A = (), B = ()> {
 	pub args_top: A,
 	pub args_sub: B,
 	pub progress: MultiProgress,
+	cleanups: Arc<RwLock<boxcar::Vec<Cleanup>>>,
 }
 
 impl Context {
@@ -17,6 +20,7 @@ impl Context {
 			args_top: (),
 			args_sub: (),
 			progress: MultiProgress::new(),
+			cleanups: Arc::new(RwLock::new(boxcar::Vec::new())),
 		}
 	}
 }
@@ -27,6 +31,7 @@ impl<A, B> Context<A, B> {
 			args_top,
 			args_sub: self.args_sub,
 			progress: self.progress,
+			cleanups: self.cleanups,
 		}
 	}
 
@@ -35,6 +40,7 @@ impl<A, B> Context<A, B> {
 			args_top: self.args_top,
 			args_sub,
 			progress: self.progress,
+			cleanups: self.cleanups,
 		}
 	}
 
@@ -45,14 +51,23 @@ impl<A, B> Context<A, B> {
 				args_top: (),
 				args_sub: self.args_sub,
 				progress: self.progress,
+				cleanups: self.cleanups,
 			},
 		)
+	}
+
+	pub fn bar(&self, len: u64) -> ProgressBar {
+		self.progress.add(ProgressBar::new(len).with_style(
+			ProgressStyle::default_bar()
+				.template("[{bar:.green/blue}] {wide_msg} {human_pos}/{human_len} ({eta})")
+				.expect("bar template invalid")
+		))
 	}
 
 	pub fn data_bar(&self, len: u64) -> ProgressBar {
 		self.progress.add(ProgressBar::new(len).with_style(
 			ProgressStyle::default_bar()
-				.template("[{bar:20.cyan/blue}] {wide_msg} {bytes}/{total_bytes} [{bytes_per_sec}] ({eta})")
+				.template("[{bar:.cyan/blue}] {wide_msg} {bytes}/{total_bytes} [{bytes_per_sec}] ({eta})")
 				.expect("data bar template invalid")
 		))
 	}
@@ -62,7 +77,21 @@ impl<A, B> Context<A, B> {
 			args_top: (),
 			args_sub: (),
 			progress: self.progress.clone(),
+			cleanups: self.cleanups.clone(),
 		}
+	}
+
+	// TODO: clean up on ctrl-c
+	pub fn add_cleanup(&self, cleanup: Cleanup) {
+		self.cleanups.read().unwrap().push(cleanup);
+	}
+
+	pub fn process_cleanups(&self) -> Vec<Cleanup> {
+		let mut guard = self.cleanups.write().unwrap();
+		let locked = guard.deref_mut();
+		let mut retrieved = boxcar::Vec::new();
+		swap(locked, &mut retrieved);
+		retrieved.into_iter().collect()
 	}
 }
 
@@ -89,4 +118,9 @@ impl<'w, A, B> MakeWriter<'w> for Context<A, B> {
 	fn make_writer_for(&'w self, _meta: &Metadata<'_>) -> Self::Writer {
 		ProgressLogWriter(self.progress.clone())
 	}
+}
+
+#[derive(Debug, Clone)]
+pub enum Cleanup {
+	MultiPartUpload(UploadId),
 }
