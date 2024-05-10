924c367636a87c7d3dc008bf6df666d83bdba248
Félix Saparelli <felix@passcod.name>
2024-05-10 18:38:46 +1200

    docs(improv-wifi): features

--- crates/improv-wifi/src/lib.rs
+++ crates/improv-wifi/src/lib.rs
@@ -1,14 +1,23 @@
 #![cfg(target_os = "linux")]
-//! An implementation of [improv-wifi](https://www.improv-wifi.com) for Linux.
+//! An implementation of [improv-wifi] for Linux.
 //!
 //! This crate provides an implementation of the Improv Wi-Fi configuration protocol, as a
 //! peripheral, via BlueZ's D-Bus API. It is intended to be used in conjunction with the
-//! [improv-wifi](https://www.improv-wifi.com) tooling for Web and Android, to allow for easy
-//! connection of an embedded Linux device to a Wi-Fi network without the need for a display or
-//! other input peripherals.
+//! [improv-wifi] tooling for Web and Android, to allow for easy connection of an embedded Linux
+//! device to a Wi-Fi network without the need for a display or other input peripherals.
 //!
-//! As there are many different network managers available for Linux, this crate provides a trait to
-//! allow any network manager to be supported.
+//! As there are many different network systems available for Linux, this crate supplies a trait to
+//! allow any network configuration framework to be supported.
 //!
-//! It also bundles an implementation for [NetworkManager](https://www.networkmanager.dev) with the
-//! optional feature `networkmanager`.
+//! If you are looking for an out-of-the-box solution, you can use the `improv-wifi-cli` application
+//! crate, which uses this crate to provide a command-line program and systemd service.
+//!
+//! # Examples
+//!
+//! # Features
+//!
+//! - `miette`: Implements `miette::Diagnostic` on the error type.
+//! - `networkmanager`: Enables the `NetworkManager` wifi configurator.
+//!
+//! [improv-wifi]: https://www.improv-wifi.com
+//! [NetworkManager]: https://www.networkmanager.dev
