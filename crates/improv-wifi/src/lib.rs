f55e1363103ebcbe6d2b679afb1122cf55b66d79
Félix Saparelli <felix@passcod.name>
2024-05-18 01:34:19 +1200

    wip: notify error/current

--- crates/improv-wifi/src/lib.rs
+++ crates/improv-wifi/src/lib.rs
@@ -19,26 +19,37 @@
 //! [improv-wifi]: https://www.improv-wifi.com
 //! [NetworkManager]: https://www.networkmanager.dev
 
-use std::{sync::{Arc, RwLock}, time::Duration};
+use std::{
+	sync::{Arc, RwLock},
+	time::Duration,
+};
 
 use bluer::{
 	gatt::local::{
-		characteristic_control, service_control, Application, ApplicationHandle, Characteristic, CharacteristicControl, CharacteristicRead, Service, ServiceControl
+		characteristic_control, service_control, Application, ApplicationHandle, Characteristic,
+		CharacteristicControl, CharacteristicNotify, CharacteristicNotifyMethod,
+		CharacteristicRead, Service, ServiceControl,
 	},
 	Adapter, Result, Uuid,
 };
 use error::Error;
 use status::Status;
+use tokio::sync::broadcast::{channel as broadcast_channel, Sender};
 
 mod error;
 mod status;
 
 const SERVICE_UUID: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268000);
-const CHARACTERISTIC_UUID_CAPABILITIES: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268005);
-const CHARACTERISTIC_UUID_CURRENT_STATE: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268001);
-const CHARACTERISTIC_UUID_ERROR_STATE: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268002);
-const CHARACTERISTIC_UUID_RPC_COMMAND: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268003);
-const CHARACTERISTIC_UUID_RPC_RESULT: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268004);
+const CHARACTERISTIC_UUID_CAPABILITIES: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268005);
+const CHARACTERISTIC_UUID_CURRENT_STATE: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268001);
+const CHARACTERISTIC_UUID_ERROR_STATE: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268002);
+const CHARACTERISTIC_UUID_RPC_COMMAND: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268003);
+const CHARACTERISTIC_UUID_RPC_RESULT: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268004);
 
 #[derive(Debug)]
 pub struct InnerState {
@@ -86,6 +97,8 @@ pub struct ImprovWifi<T> {
 	timeout: Option<Duration>,
 	state: State,
 	handler: T,
+	status_change_notifier: Sender<()>,
+	error_change_notifier: Sender<()>,
 	app: ApplicationHandle,
 	service: ServiceControl,
 	capabilities: CharacteristicControl,
@@ -102,29 +115,22 @@ pub trait WifiConfigurator {
 }
 
 impl<T: WifiConfigurator> ImprovWifi<T> {
-	fn notify_error(&self) {
-		let error_byte = self.state.last_error().map_or(0, |e| e.as_byte());
-		todo!("write and notify the error to a connected client");
-	}
-
-	fn notify_status(&self) {
-		let state_byte = self.state.status().as_byte();
-		todo!("write and notify the state to a connected client");
-	}
-
 	fn modify_status(&mut self, status: Status) {
 		self.state.set_status(status);
-		self.notify_status();
+		self.status_change_notifier.send(()).ok();
+		todo!("write the state to a connected client");
 	}
 
 	pub fn set_error(&mut self, error: Error) {
 		self.state.set_last_error(Some(error));
-		self.notify_error();
+		self.error_change_notifier.send(()).ok();
+		todo!("write the error to a connected client");
 	}
 
 	pub fn clear_error(&mut self) {
 		self.state.set_last_error(None);
-		self.notify_error();
+		self.error_change_notifier.send(()).ok();
+		todo!("write the error to a connected client");
 	}
 
 	pub fn set_authorized(&mut self) {
@@ -163,6 +169,8 @@ impl<T: WifiConfigurator> ImprovWifi<T> {
 			status: initial_status,
 			last_error: None,
 		});
+		let (status_change_notifier, _) = broadcast_channel(2);
+		let (error_change_notifier, _) = broadcast_channel(2);
 
 		let (service, service_handle) = service_control();
 		let (capabilities_control, capabilities_handle) = characteristic_control();
@@ -182,11 +190,7 @@ impl<T: WifiConfigurator> ImprovWifi<T> {
 							read: true,
 							fun: Box::new(move |_| {
 								Box::pin(async move {
-									let byte = if T::can_identify() {
-										1
-									} else {
-										0
-									};
+									let byte = if T::can_identify() { 1 } else { 0 };
 									Ok(vec![byte])
 								})
 							}),
@@ -202,9 +206,33 @@ impl<T: WifiConfigurator> ImprovWifi<T> {
 							fun: Box::new({
 								let state = state.clone();
 								move |_| {
+									let state = state.clone();
+									Box::pin(async move { Ok(vec![state.status().as_byte()]) })
+								}
+							}),
+							..Default::default()
+						}),
+						notify: Some(CharacteristicNotify {
+							notify: true,
+							method: CharacteristicNotifyMethod::Fun(Box::new({
 								let state = state.clone();
-								Box::pin(async move { Ok(vec![state.status().as_byte()]) })
-							}}),
+								let status_change_notifier = status_change_notifier.clone();
+								move |mut notifier| {
+									let state = state.clone();
+									let mut status_change_receiver =
+										status_change_notifier.subscribe();
+									Box::pin(async move {
+										tokio::spawn(async move {
+											while let Ok(()) = status_change_receiver.recv().await {
+												notifier
+													.notify(vec![state.status().as_byte()])
+													.await
+													.ok();
+											}
+										});
+									})
+								}
+							})),
 							..Default::default()
 						}),
 						control_handle: current_handle,
@@ -217,11 +245,37 @@ impl<T: WifiConfigurator> ImprovWifi<T> {
 							fun: Box::new({
 								let state = state.clone();
 								move |_| {
+									let state = state.clone();
+									Box::pin(async move {
+										Ok(vec![state.last_error().map_or(0x00, |s| s.as_byte())])
+									})
+								}
+							}),
+							..Default::default()
+						}),
+						notify: Some(CharacteristicNotify {
+							notify: true,
+							method: CharacteristicNotifyMethod::Fun(Box::new({
 								let state = state.clone();
-								Box::pin(async move {
-									Ok(vec![state.last_error().map_or(0x00, |s| s.as_byte())])
-								})
-							}}),
+								let error_change_notifier = error_change_notifier.clone();
+								move |mut notifier| {
+									let state = state.clone();
+									let mut error_change_receiver =
+										error_change_notifier.subscribe();
+									Box::pin(async move {
+										tokio::spawn(async move {
+											while let Ok(()) = error_change_receiver.recv().await {
+												notifier
+													.notify(vec![state
+														.last_error()
+														.map_or(0x00, |s| s.as_byte())])
+													.await
+													.ok();
+											}
+										});
+									})
+								}
+							})),
 							..Default::default()
 						}),
 						control_handle: error_handle,
@@ -252,6 +306,8 @@ impl<T: WifiConfigurator> ImprovWifi<T> {
 			},
 			state,
 			handler,
+			status_change_notifier,
+			error_change_notifier,
 			app: adapter.serve_gatt_application(app).await?,
 			service,
 			capabilities: capabilities_control,
