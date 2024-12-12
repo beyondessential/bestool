1d98fe94ec2125f46ca38b5a5170feda3caad609
rail-rain <takayuki@bes.au>
2024-12-12 14:41:44 +1300

    Do encrypting and decrypting in stream

--- crates/bestool/src/actions/crypto/decrypt.rs
+++ crates/bestool/src/actions/crypto/decrypt.rs
@@ -1,8 +1,10 @@
-use std::path::PathBuf;
+use std::{iter, path::PathBuf};
 
-use age::x25519;
+use age::{x25519, Decryptor};
 use clap::Parser;
 use miette::{miette, Context as _, IntoDiagnostic as _, Result};
+use tokio::io::AsyncWriteExt;
+use tokio_util::compat::{FuturesAsyncReadCompatExt as _, TokioAsyncReadCompatExt as _};
 
 use crate::actions::{crypto::CryptoArgs, Context};
 
@@ -22,16 +24,32 @@ pub async fn run(ctx: Context<CryptoArgs, DecryptArgs>) -> Result<()> {
 		.parse()
 		.map_err(|err: &str| miette!("failed to parse: {err}"))?;
 
-	let encrypted = tokio::fs::read(&ctx.args_sub.encrypted)
+	let encrypted = tokio::fs::File::open(&ctx.args_sub.encrypted)
 		.await
 		.into_diagnostic()
-		.wrap_err("reading the encrypted file")?;
+		.wrap_err("opening the encrypted file")?;
 
-	let plaintext = age::decrypt(&private_key, &encrypted).into_diagnostic()?;
-	tokio::fs::write(ctx.args_sub.encrypted.with_extension(""), plaintext)
+	let mut plaintext = tokio::fs::File::create_new(ctx.args_sub.encrypted.with_extension(""))
 		.await
 		.into_diagnostic()
-		.wrap_err("writing the decrypted data")?;
+		.wrap_err("opening the decrypted output")?;
 
+	let mut decrypting_reader = Decryptor::new_async(encrypted.compat())
+		.await
+		.into_diagnostic()?
+		.decrypt_async(iter::once(&private_key as _))
+		.into_diagnostic()?
+		.compat();
+
+	tokio::io::copy(&mut decrypting_reader, &mut plaintext)
+		.await
+		.into_diagnostic()
+		.wrap_err("decrypting data in stream")?;
+
+	plaintext
+		.shutdown()
+		.await
+		.into_diagnostic()
+		.wrap_err("closing the plaintext output")?;
 	Ok(())
 }
--- crates/bestool/src/actions/crypto/encrypt.rs
+++ crates/bestool/src/actions/crypto/encrypt.rs
@@ -1,8 +1,10 @@
-use std::path::PathBuf;
+use std::{iter, path::PathBuf};
 
-use age::x25519;
+use age::{x25519, Encryptor};
 use clap::Parser;
 use miette::{miette, Context as _, IntoDiagnostic as _, Result};
+use tokio::io::AsyncWriteExt;
+use tokio_util::compat::{FuturesAsyncWriteCompatExt as _, TokioAsyncWriteCompatExt as _};
 
 use crate::actions::{crypto::CryptoArgs, Context};
 
@@ -22,18 +24,35 @@ pub async fn run(ctx: Context<CryptoArgs, EncryptArgs>) -> Result<()> {
 		.parse()
 		.map_err(|err: &str| miette!("failed to parse: {err}"))?;
 
-	let plaintext = tokio::fs::read(&ctx.args_sub.plaintext)
+	let mut plaintext = tokio::fs::File::open(&ctx.args_sub.plaintext)
 		.await
 		.into_diagnostic()
-		.wrap_err("reading the plainetxt")?;
-	let encrypted = age::encrypt(&public_key, &plaintext).into_diagnostic()?;
+		.wrap_err("opening the plainetxt")?;
 
 	let mut encrypted_path = ctx.args_sub.plaintext.into_os_string();
 	encrypted_path.push(".enc");
-	tokio::fs::write(encrypted_path, encrypted)
+	let encrypted = tokio::fs::File::create_new(encrypted_path)
 		.await
 		.into_diagnostic()
-		.wrap_err("writing the encrypted data")?;
+		.wrap_err("opening the encrypted output")?;
+
+	let mut encrypting_writer = Encryptor::with_recipients(iter::once(&public_key as _))
+		.expect("a recipient should exist")
+		.wrap_async_output(encrypted.compat_write())
+		.await
+		.into_diagnostic()?
+		.compat_write();
+
+	tokio::io::copy(&mut plaintext, &mut encrypting_writer)
+		.await
+		.into_diagnostic()
+		.wrap_err("encrypting data in stream")?;
+
+	encrypting_writer
+		.shutdown()
+		.await
+		.into_diagnostic()
+		.wrap_err("closing the encrypted output")?;
 
 	Ok(())
 }
