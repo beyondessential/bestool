a39d39d67f7e8ad7c7777cfc1732471c1ee249a9
Félix Saparelli <felix@passcod.name>
2024-12-23 21:08:45 +1300

    feat(crypto): write identity.pub by default

--- crates/bestool/src/actions/crypto/keygen.rs
+++ crates/bestool/src/actions/crypto/keygen.rs
@@ -18,7 +18,8 @@ use super::keys::PassphraseArgs;
 
 /// Generate a key pair to encrypt and decrypt files with.
 ///
-/// This creates a single identity file which contains a public and secret key:
+/// This creates a passphrase-protected identity file which contains both public
+/// and secret keys:
 ///
 /// ```identity.txt
 /// # created: 2024-12-20T05:36:10.267871872+00:00
@@ -26,19 +27,22 @@ use super::keys::PassphraseArgs;
 /// AGE-SECRET-KEY-1N84CR29PJTUQA22ALHP4YDL5ZFMXPW5GVETVY3UK58ZD6NPNPDLS4MCZFS
 /// ```
 ///
+/// As well as a plaintext public key file which contains just the public key:
+///
+/// ```identity.pub
+/// age1c3jdepjm05aey2dq9dgkfn4utj9a776zwqzqcar3879smuh04ysqttvmyd
+/// ```
+///
+/// The public key is also printed to stdout.
+///
 /// By default this command prompts for a passphrase. This can be disabled with
 /// `--plaintext`; the default path `identity.txt` instead of `identity.txt.age`
 /// is used if `--output` isn't given, and the contents will be in plain text
 /// (in the format shown above).
 ///
 /// On encrypting machines (e.g. servers uploading backups), you should always
-/// prefer to store _just_ the public key, in a separate file as below, and then
-/// upload and use the passphrase-protected identity file as necessary, deleting
-/// it afterwards.
-///
-/// ```key.pub
-/// age1c3jdepjm05aey2dq9dgkfn4utj9a776zwqzqcar3879smuh04ysqttvmyd
-/// ```
+/// prefer to store _just_ the public key, and only upload and use the
+/// passphrase-protected identity file as necessary, deleting it afterwards.
 ///
 /// Identity files (both plaintext and passphrase-protected) are compatible with
 /// the `age` CLI tool. Note that the reverse might not be true.
@@ -48,9 +52,17 @@ pub struct KeygenArgs {
 	/// Path to write the identity file to.
 	///
 	/// Defaults to identity.txt.age, and to identity.txt if --plaintext is given.
-	#[cfg_attr(docsrs, doc("\n\n**Flag**: `--output PATH`"))]
+	#[cfg_attr(docsrs, doc("\n\n**Flag**: `-o, --output PATH`"))]
 	#[arg(short, long)]
 	pub output: Option<PathBuf>,
+
+	/// Path to write the public key file to.
+	///
+	/// Set to a single hyphen (`-`) to disable writing this file; the public key
+	/// will be printed to stdout in any case.
+	#[cfg_attr(docsrs, doc("\n\n**Flag**: `--public PATH`"))]
+	#[arg(long = "public", default_value = "identity.pub")]
+	pub public_path: PathBuf,
 
 	/// INSECURE: write a plaintext identity.
 	#[cfg_attr(docsrs, doc("\n\n**Flag**: `--plaintext`"))]
@@ -66,6 +78,7 @@ pub async fn run(ctx: Context<CryptoArgs, KeygenArgs>) -> Result<()> {
 		output,
 		plaintext,
 		key,
+		public_path,
 	} = ctx.args_sub;
 
 	let secret = x25519::Identity::generate();
@@ -102,6 +115,18 @@ pub async fn run(ctx: Context<CryptoArgs, KeygenArgs>) -> Result<()> {
 		.await
 		.into_diagnostic()
 		.wrap_err("writing the identity")?;
+
+	println!("public key: {public}");
+	if public_path.to_string_lossy() != "-" {
+		File::create_new(&public_path)
+			.await
+			.into_diagnostic()
+			.wrap_err("opening the public key file")?
+			.write_all(public.to_string().as_bytes())
+			.await
+			.into_diagnostic()
+			.wrap_err("writing the public key")?;
+	}
 
 	Ok(())
 }
