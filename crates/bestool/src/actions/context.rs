0297fdc3bb30584e3cb28effdf3645f8a3b5197a
Félix Saparelli <felix@passcod.name>
2025-02-03 14:20:52 +1300

    refactor: use lloggs instead of custom logging code

--- crates/bestool/src/actions/context.rs
+++ crates/bestool/src/actions/context.rs
@@ -1,14 +1,7 @@
-use std::io::{stderr, Write};
-
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
-use tracing::Metadata;
-use tracing_subscriber::fmt::MakeWriter;
-
 #[derive(Clone, Debug)]
 pub struct Context<A = (), B = ()> {
 	pub args_top: A,
 	pub args_sub: B,
-	pub progress: MultiProgress,
 }
 
 impl Context {
@@ -16,7 +9,6 @@ impl Context {
 		Self {
 			args_top: (),
 			args_sub: (),
-			progress: MultiProgress::new(),
 		}
 	}
 }
@@ -27,7 +19,6 @@ impl<A, B> Context<A, B> {
 		Context::<C, B> {
 			args_top,
 			args_sub: self.args_sub,
-			progress: self.progress,
 		}
 	}
 
@@ -35,7 +26,6 @@ impl<A, B> Context<A, B> {
 		Context::<A, C> {
 			args_top: self.args_top,
 			args_sub,
-			progress: self.progress,
 		}
 	}
 
@@ -43,7 +33,6 @@ impl<A, B> Context<A, B> {
 		Context::<B, C> {
 			args_top: self.args_sub,
 			args_sub: new_sub,
-			progress: self.progress,
 		}
 	}
 
@@ -53,59 +42,14 @@ impl<A, B> Context<A, B> {
 			Context::<(), B> {
 				args_top: (),
 				args_sub: self.args_sub,
-				progress: self.progress,
 			},
 		)
-	}
-
-	pub fn bar(&self, len: u64) -> ProgressBar {
-		self.progress.add(
-			ProgressBar::new(len).with_style(
-				ProgressStyle::default_bar()
-					.template("[{bar:.green/blue}] {wide_msg} {human_pos}/{human_len} ({eta})")
-					.expect("bar template invalid"),
-			),
-		)
-	}
-
-	pub fn data_bar(&self, len: u64) -> ProgressBar {
-		self.progress.add(ProgressBar::new(len).with_style(
-			ProgressStyle::default_bar()
-				.template("[{bar:.cyan/blue}] {wide_msg} {bytes}/{total_bytes} [{bytes_per_sec}] ({eta})")
-				.expect("data bar template invalid")
-		))
 	}
 
 	pub fn erased(&self) -> Context<(), ()> {
 		Context::<(), ()> {
 			args_top: (),
 			args_sub: (),
-			progress: self.progress.clone(),
 		}
-	}
-}
-
-#[derive(Debug, Clone)]
-pub struct ProgressLogWriter(MultiProgress);
-
-impl Write for ProgressLogWriter {
-	fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-		self.0.suspend(|| stderr().write(buf))
-	}
-
-	fn flush(&mut self) -> std::io::Result<()> {
-		self.0.suspend(|| stderr().flush())
-	}
-}
-
-impl<'w, A, B> MakeWriter<'w> for Context<A, B> {
-	type Writer = ProgressLogWriter;
-
-	fn make_writer(&'w self) -> Self::Writer {
-		ProgressLogWriter(self.progress.clone())
-	}
-
-	fn make_writer_for(&'w self, _meta: &Metadata<'_>) -> Self::Writer {
-		ProgressLogWriter(self.progress.clone())
 	}
 }
--- crates/bestool/src/args.rs
+++ crates/bestool/src/args.rs
@@ -1,10 +1,7 @@
-use std::{env::var, fs::metadata, io::stderr, path::PathBuf};
-
-use clap::{ArgAction, Parser, ValueEnum, ValueHint};
-use miette::{bail, Result};
-use tracing::{debug, warn};
-use tracing_appender::{non_blocking, non_blocking::WorkerGuard, rolling};
-use tracing_subscriber::EnvFilter;
+use clap::Parser;
+use lloggs::{LoggingArgs, PreArgs, WorkerGuard};
+use miette::{miette, Result};
+use tracing::debug;
 
 /// BES Tooling
 #[derive(Debug, Clone, Parser)]
@@ -16,184 +13,36 @@ use tracing_subscriber::EnvFilter;
 )]
 #[cfg_attr(debug_assertions, command(before_help = "⚠ DEBUG BUILD ⚠"))]
 pub struct Args {
-	/// When to use terminal colours
-	///
-	/// You can also set the NO_COLOR environment variable to disable colours.
-	#[arg(long, default_value = "auto", value_name = "MODE", alias = "colour")]
-	pub color: ColourMode,
-
-	/// Set diagnostic log level
-	///
-	/// This enables diagnostic logging, which is useful for investigating bugs. Use multiple
-	/// times to increase verbosity. Goes up to '-vvvvv'.
-	///
-	/// You may want to use with '--log-file' to avoid polluting your terminal.
-	///
-	/// Setting $RUST_LOG also works, and takes precedence, but is not recommended unless you know
-	/// what you're doing. However, using $RUST_LOG is the only way to get logs from before these
-	/// options are parsed.
-	#[arg(
-		long,
-		short,
-		action = ArgAction::Count,
-		num_args = 0,
-		default_value = "0",
-	)]
-	pub verbose: u8,
-
-	/// Write diagnostic logs to a file
-	///
-	/// This writes diagnostic logs to a file, instead of the terminal, in JSON format. If a log
-	/// level was not already specified, this will set it to '-vvv'.
-	///
-	/// If the path provided is a directory, a file will be created in that directory. The file name
-	/// will be the current date and time, in the format 'bestool.YYYY-MM-DDTHH-MM-SSZ.log'.
-	#[arg(
-		long,
-		num_args = 0..=1,
-		default_missing_value = ".",
-		value_hint = ValueHint::AnyPath,
-		value_name = "PATH",
-	)]
-	pub log_file: Option<PathBuf>,
-
-	/// Omit timestamps in logs
-	///
-	/// This can be useful when running under systemd, to avoid having two timestamps.
-	///
-	/// This option is ignored if the log file is set, or when using $RUST_LOG (as logging is
-	/// initialized before arguments are parsed in that case).
-	#[arg(long)]
-	pub log_timeless: bool,
+	#[command(flatten)]
+	logging: LoggingArgs,
 
 	/// What to do
 	#[command(subcommand)]
 	pub action: crate::actions::Action,
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, ValueEnum)]
-pub enum ColourMode {
-	Auto,
-	Always,
-	Never,
-}
-
 pub fn get_args() -> Result<(Args, WorkerGuard)> {
-	let prearg_log_guard = logging_preargs();
-	if prearg_log_guard.is_some() {
-		warn!("⚠ RUST_LOG environment variable set or hardcoded, logging options have no effect");
-	}
+	let log_guard = PreArgs::parse().setup().map_err(|err| miette!("{err}"))?;
 
 	debug!("parsing arguments");
-	let mut args = Args::parse();
+	let args = Args::parse();
 
-	let log_guard = match prearg_log_guard {
+	let log_guard = match log_guard {
 		Some(g) => g,
-		None => logging_postargs(&args)?,
+		None => args
+			.logging
+			.setup(|v| match v {
+				0 => "info",
+				1 => "info,bestool=debug",
+				2 => "debug",
+				3 => "debug,bestool=trace",
+				_ => "trace",
+			})
+			.map_err(|err| miette!("{err}"))?,
 	};
-
-	// https://no-color.org/
-	if var("NO_COLOR").is_ok() {
-		debug!("NO_COLOR environment variable set, ignoring --color option");
-		args.color = ColourMode::Never;
-	} else if enable_ansi_support::enable_ansi_support().is_err() {
-		debug!("failed to enable colour support on Windows, disabling colour");
-		args.color = ColourMode::Never;
-	}
 
 	debug!(?args, "got arguments");
 	Ok((args, log_guard))
-}
-
-pub fn logging_preargs() -> Option<WorkerGuard> {
-	let Ok(rust_log) = var("RUST_LOG") else {
-		return None;
-	};
-
-	let (writer, guard) = non_blocking(stderr());
-
-	let builder = tracing_subscriber::fmt()
-		.with_env_filter(EnvFilter::from_default_env())
-		.with_writer(writer)
-		.try_init();
-
-	match builder {
-		Ok(()) => {
-			warn!(RUST_LOG=%rust_log, "logging configured from RUST_LOG");
-			Some(guard)
-		}
-		Err(e) => {
-			eprintln!("Failed to initialise logging with RUST_LOG, falling back\n{e}");
-			None
-		}
-	}
-}
-
-pub fn logging_postargs(args: &Args) -> Result<WorkerGuard> {
-	let (log_writer, guard) = if let Some(file) = &args.log_file {
-		let is_dir = metadata(&file).map_or(false, |info| info.is_dir());
-		let (dir, filename) = if is_dir {
-			(
-				file.to_owned(),
-				PathBuf::from(format!(
-					"bestool.{}.log",
-					chrono::Utc::now().format("%Y-%m-%dT%H-%M-%SZ")
-				)),
-			)
-		} else if let (Some(parent), Some(file_name)) = (file.parent(), file.file_name()) {
-			(parent.into(), PathBuf::from(file_name))
-		} else {
-			bail!("Failed to determine log file name");
-		};
-
-		non_blocking(rolling::never(dir, filename))
-	} else {
-		non_blocking(stderr())
-	};
-
-	let mut builder = tracing_subscriber::fmt().with_env_filter(match args.verbose {
-		0 => "info",
-		1 => "info,bestool=debug",
-		2 => "debug",
-		3 => "debug,bestool=trace",
-		_ => "trace",
-	});
-
-	match args.color {
-		ColourMode::Never => {
-			builder = builder.with_ansi(false);
-		}
-		ColourMode::Always => {
-			builder = builder.with_ansi(true);
-		}
-		ColourMode::Auto => {}
-	}
-
-	if args.verbose > 0 {
-		use tracing_subscriber::fmt::format::FmtSpan;
-		builder = builder.with_span_events(FmtSpan::NEW | FmtSpan::CLOSE);
-	}
-
-	match if args.log_file.is_some() {
-		builder.json().with_writer(log_writer).try_init()
-	} else if args.verbose > 3 {
-		if args.log_timeless {
-			builder.without_time().with_writer(log_writer).try_init()
-		} else {
-			builder.pretty().with_writer(log_writer).try_init()
-		}
-	} else {
-		if args.log_timeless {
-			builder.without_time().with_writer(log_writer).try_init()
-		} else {
-			builder.with_writer(log_writer).try_init()
-		}
-	} {
-		Ok(()) => debug!("logging initialised"),
-		Err(e) => eprintln!("Failed to initialise logging, continuing with none\n{e}"),
-	}
-
-	Ok(guard)
 }
 
 #[test]
