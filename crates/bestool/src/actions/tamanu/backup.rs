5b0083ed4d4e2a1e48543021a0302dcfadd5bd8c
Félix Saparelli <felix@passcod.name>
2025-03-05 02:36:03 +1300

    chunk files

--- crates/bestool/src/actions/tamanu/backup.rs
+++ crates/bestool/src/actions/tamanu/backup.rs
@@ -1,5 +1,7 @@
 use std::{
 	ffi::{OsStr, OsString},
+	io::{stderr, IsTerminal as _},
+	num::NonZero,
 	path::{Path, PathBuf},
 	time::{Duration, SystemTime},
 };
@@ -10,10 +12,11 @@ use algae_cli::{
 };
 use chrono::Utc;
 use clap::Parser;
+use indicatif::{ProgressBar, ProgressStyle};
 use miette::{Context as _, IntoDiagnostic as _, Result};
 use tokio::{
 	fs::{self, create_dir_all},
-	io::AsyncWriteExt as _,
+	io::{AsyncReadExt as _, AsyncWriteExt as _},
 };
 use tracing::{debug, info, instrument, warn};
 
@@ -56,6 +59,29 @@ pub struct BackupArgs {
 	/// The backup will stay as is in "write_to".
 	#[arg(long)]
 	pub then_copy_to: Option<PathBuf>,
+
+	/// Split the copied file into fixed-sized chunks.
+	///
+	/// Backups can be very large files. Uploading them in one go over an unreliable connection can
+	/// be a painful experience, and in some cases not succeed. This option provides a lo-fi
+	/// solution to the problem, by splitting the file created by `--then-copy` into smaller chunks.
+	/// It is then a lot easier to upload the chunks and retry on error or after network failures by
+	/// re-uploading chunks missing on the remote; `rclone sync` can do this for example.
+	///
+	/// The file chunks are written into a directory named after the original file, including the
+	/// extension. This makes the other side simpler: take all the chunks and re-assemble into one
+	/// file, naming it the same as the containing directory.
+	///
+	/// A metadata file is also written. This is a JSON file which contains the number of chunks
+	/// created, a checksum over the whole file, and a checksum for each chunk. This can be used by
+	/// the far-side re-assembler to check whether all files are available, and verify integrity.
+	///
+	/// Splitting happens after encryption, if enabled.
+	///
+	/// Takes a non-zero integer size in mibibytes, or `0`, which will pick either 64MiB or 1000
+	/// chunks (rounded to 8192 bytes), whichever makes smaller chunks, with a minimum size of 8MiB.
+	#[arg(long)]
+	pub then_split: Option<u16>,
 
 	/// Take a lean backup instead.
 	///
@@ -172,7 +198,18 @@ pub async fn run(ctx: Context<TamanuArgs, BackupArgs>) -> Result<()> {
 	process_backup(
 		output,
 		&ctx.args_sub.write_to,
-		ctx.args_sub.then_copy_to.as_deref(),
+		ctx.args_sub.then_copy_to.as_deref().map(|path| Then {
+			copy_to: path,
+			split: ctx
+				.args_sub
+				.then_split
+				.map(|n| {
+					NonZero::new(n)
+						.map(ThenSplit::Mib)
+						.unwrap_or(ThenSplit::Auto)
+				})
+				.unwrap_or(ThenSplit::No),
+		}),
 		ctx.args_sub.keep_days,
 		".dump",
 		ctx.args_sub.key,
@@ -182,10 +219,57 @@ pub async fn run(ctx: Context<TamanuArgs, BackupArgs>) -> Result<()> {
 	Ok(())
 }
 
+#[derive(Debug, Clone, Copy)]
+pub(crate) enum ThenSplit {
+	No,
+	Auto,
+	Mib(NonZero<u16>),
+}
+
+const MIBIBYTE: u64 = 1024_u64.pow(3);
+const MAX_AUTO_CHUNKS: u64 = 1000;
+const MINPAGE: u64 = 8192;
+// We round chunk sizes so they always fall on the disk page size for best write and storage perf
+
+impl ThenSplit {
+	// None if we're not splitting
+	fn max_chunk_bytes(self, full_size: u64) -> Option<u64> {
+		match self {
+			Self::No => None,
+			Self::Mib(mib) => Some({
+				let chunk_bytes = u64::from(mib.get()) * MIBIBYTE;
+				if full_size < chunk_bytes {
+					full_size
+				} else {
+					chunk_bytes
+				}
+			}),
+			Self::Auto => {
+				// SAFETY: constants
+				// UNWRAP: Mib always returns Some
+				let if_8_mib = Self::Mib(unsafe { NonZero::new_unchecked(8) })
+					.max_chunk_bytes(full_size)
+					.unwrap();
+				let if_64_mib = Self::Mib(unsafe { NonZero::new_unchecked(64) })
+					.max_chunk_bytes(full_size)
+					.unwrap();
+				let if_max_chunks = (full_size / MAX_AUTO_CHUNKS / MINPAGE) * MINPAGE;
+				Some(if_max_chunks.min(if_64_mib).max(if_8_mib))
+			}
+		}
+	}
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct Then<'a> {
+	pub copy_to: &'a Path,
+	pub split: ThenSplit,
+}
+
 pub(crate) async fn process_backup(
 	output: PathBuf,
 	written_to: &Path,
-	then_copy_to: Option<&Path>,
+	then: Option<Then<'_>>,
 	keep_days: Option<u16>,
 	purge_extension: &str,
 	key: KeyArgs,
@@ -206,23 +290,38 @@ pub(crate) async fn process_backup(
 		output
 	};
 
+	let full_size = {
+		let meta = fs::metadata(&output)
+			.await
+			.into_diagnostic()
+			.wrap_err("opening the output (metadata)")?;
+		meta.len()
+	};
+	info!("wrote {full_size} bytes");
+
 	let output_filename = output
 		.file_name()
 		.expect("from above we know it's got a filename");
 
-	if let Some(then_copy_to) = then_copy_to {
-		create_dir_all(then_copy_to)
+	if let Some(Then { copy_to, split }) = then {
+		create_dir_all(copy_to)
 			.await
 			.into_diagnostic()
 			.wrap_err("creating copy dest dir")?;
 
-		let target_path = then_copy_to.join(output_filename);
-		info!(from=?output, to=?target_path, "copying backup");
+		let target_path = copy_to.join(output_filename);
 
-		// attempt copy first via fs, then fallback to via io
-		if let Err(e) = fs::copy(&output, &target_path).await {
-			warn!(?e, "fs::copy failed, falling back to io::copy");
-			copy_via_io(&output, target_path).await?;
+		if let Some(chunk_size) = split.max_chunk_bytes(full_size) {
+			info!(from=?output, to=?target_path, %chunk_size, "copying split backup");
+			copy_into_chunks(&output, full_size, target_path, chunk_size).await?;
+		} else {
+			info!(from=?output, to=?target_path, "copying whole backup");
+
+			// attempt copy first via fs, then fallback to via io
+			if let Err(e) = fs::copy(&output, &target_path).await {
+				warn!(?e, "fs::copy failed, falling back to io::copy");
+				copy_via_io(&output, full_size, target_path).await?;
+			}
 		}
 	}
 
@@ -231,7 +330,7 @@ pub(crate) async fn process_backup(
 			.await
 			.wrap_err("purging old backups in main target")?;
 
-		if let Some(copies) = then_copy_to {
+		if let Some(copies) = then.map(|t| t.copy_to) {
 			purge_old_backups(days, copies, output_filename, purge_extension)
 				.await
 				.wrap_err("purging old backups in secondary target")?;
@@ -241,17 +340,15 @@ pub(crate) async fn process_backup(
 	Ok(output)
 }
 
-async fn copy_via_io(output: &PathBuf, target_path: PathBuf) -> Result<(), miette::Error> {
-	let input = fs::File::open(output)
+async fn copy_via_io(
+	input: &PathBuf,
+	input_length: u64,
+	target_path: PathBuf,
+) -> Result<(), miette::Error> {
+	let input = fs::File::open(input)
 		.await
 		.into_diagnostic()
 		.wrap_err("opening the original")?;
-	let input_length = input
-		.metadata()
-		.await
-		.into_diagnostic()
-		.wrap_err("reading original file length")?
-		.len();
 
 	let mut writer = fs::File::create_new(target_path)
 		.await
@@ -271,6 +368,85 @@ async fn copy_via_io(output: &PathBuf, target_path: PathBuf) -> Result<(), miett
 		.await
 		.into_diagnostic()
 		.wrap_err("closing the target file")
+}
+
+#[derive(Debug, serde::Serialize)]
+struct ChunkedMetadata {
+	n: u64,
+	chunks: Vec<String>,
+}
+
+async fn copy_into_chunks(
+	input: &PathBuf,
+	input_length: u64,
+	target_dir: PathBuf,
+	chunk_size: u64,
+) -> Result<(), miette::Error> {
+	let mut input = fs::File::open(input)
+		.await
+		.into_diagnostic()
+		.wrap_err("opening the original")?;
+
+	let n_chunks = input_length.div_ceil(chunk_size);
+	let chunk_digits = usize::try_from(n_chunks.ilog10() + 1).unwrap();
+	let mut chunks = Vec::with_capacity(usize::try_from(n_chunks).unwrap());
+
+	let pb = if stderr().is_terminal() {
+		let style = ProgressStyle::default_bar()
+			.template("[{bar:.green/blue}] {wide_msg} {binary_bytes}/{binary_total_bytes} ({eta})")
+			.expect("BUG: progress bar template invalid");
+		ProgressBar::new(input_length).with_style(style)
+	} else {
+		ProgressBar::hidden()
+	};
+
+	let mut chunk_n = 0;
+	loop {
+		chunk_n += 1;
+		let mut chunk = input.take(chunk_size);
+		let chunk_name = format!("{chunk_n:0chunk_digits$}.chunk");
+		chunks.push(chunk_name.clone());
+		let target_path = target_dir.join(&chunk_name);
+		pb.set_message(chunk_name);
+
+		let mut writer = fs::File::create_new(&target_path)
+			.await
+			.into_diagnostic()
+			.wrap_err("opening the target file")?;
+
+		// TODO: checksums
+		let bytes = tokio::io::copy(&mut chunk, &mut writer)
+			.await
+			.into_diagnostic()
+			.wrap_err("copying data in stream")?;
+		debug!(%chunk_n, %n_chunks, "copied {bytes} bytes");
+		pb.inc(bytes);
+
+		writer
+			.shutdown()
+			.await
+			.into_diagnostic()
+			.wrap_err("closing the target file")?;
+		input = chunk.into_inner();
+
+		if bytes == 0 {
+			let _ = fs::remove_file(target_path).await;
+			break;
+		}
+	}
+
+	let meta = ChunkedMetadata {
+		n: n_chunks,
+		chunks,
+	};
+	let meta = serde_json::to_vec_pretty(&meta).unwrap();
+	fs::write(target_dir.join("metadata.json"), meta)
+		.await
+		.into_diagnostic()
+		.wrap_err("write metadata file")?;
+
+	pb.finish_with_message(format!("wrote {n_chunks} chunks"));
+	Ok(())
 }
 
 #[instrument(level = "debug")]
@@ -342,10 +518,17 @@ async fn purge_old_backups(
 		}
 
 		info!(?path, "deleting old backup");
-		fs::remove_file(&path)
-			.await
-			.into_diagnostic()
-			.wrap_err(format!("deleting {path:?}"))?;
+		if meta.is_dir() {
+			fs::remove_dir_all(&path)
+				.await
+				.into_diagnostic()
+				.wrap_err(format!("deleting dir {path:?}"))?;
+		} else {
+			fs::remove_file(&path)
+				.await
+				.into_diagnostic()
+				.wrap_err(format!("deleting file {path:?}"))?;
+		}
 	}
 
 	Ok(())
