a7394f1f6295a64e1e0f0e008fb0c236939910f5
Félix Saparelli <felix@passcod.name>
2025-07-09 16:05:28 +1200

    filter by platform

--- crates/bestool/src/actions/tamanu/artifacts.rs
+++ crates/bestool/src/actions/tamanu/artifacts.rs
@@ -1,8 +1,11 @@
 use clap::Parser;
 use comfy_table::{modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, ContentArrangement, Table};
+use detect_targets::detect_targets;
 use miette::{IntoDiagnostic, Result};
 use reqwest::Client;
 use serde::Deserialize;
+use std::str::FromStr;
+use target_tuples::{Target, OS};
 
 use crate::actions::Context;
 
@@ -16,19 +19,43 @@ pub struct ArtifactsArgs {
 	/// Version to list artifacts for.
 	#[arg(value_name = "VERSION")]
 	pub version: String,
+
+	/// Platform to list artifacts for.
+	///
+	/// Use `host` (default) for the auto-detected current platform, `container` for container artifacts,
+	/// `os-arch` for specific targets (e.g., `linux-x86_64`), and `all` to list all platforms.
+	#[arg(short, long, value_name = "PLATFORM", default_value = "host")]
+	pub platform: Platform,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Platform {
+	All,
+	Host,
+	Match(String),
+}
+
+impl FromStr for Platform {
+	type Err = String;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match s.to_ascii_lowercase().as_str() {
+			"all" => Ok(Platform::All),
+			"host" => Ok(Platform::Host),
+			s => Ok(Platform::Match(s.to_string())),
+		}
+	}
 }
 
 #[derive(Debug, Deserialize)]
-struct Artifact {
+pub struct Artifact {
 	pub artifact_type: String,
 	pub platform: String,
 	pub download_url: String,
 }
 
-pub async fn run(ctx: Context<TamanuArgs, ArtifactsArgs>) -> Result<()> {
-	let ArtifactsArgs { version } = ctx.args_sub;
-
-	let url = format!("https://meta.tamanu.app/versions/{}/artifacts", version);
+pub async fn get_artifacts(version: &str, for_platform: &Platform) -> Result<Vec<Artifact>> {
+	let url = format!("https://meta.tamanu.app/versions/{version}/artifacts");
 	let client = Client::new();
 
 	let response = client.get(&url).send().await.into_diagnostic()?;
@@ -44,11 +71,70 @@ pub async fn run(ctx: Context<TamanuArgs, ArtifactsArgs>) -> Result<()> {
 	let mut artifacts: Vec<Artifact> = response.json().await.into_diagnostic()?;
 	if artifacts.is_empty() {
 		println!("No artifacts found for version {}", version);
-		return Ok(());
+		return Ok(Vec::new());
 	}
+
+	let host_targets = detect_targets()
+		.await
+		.into_iter()
+		.filter_map(|target| Target::from_str(&target).ok())
+		.collect::<Vec<Target>>();
+
+	artifacts.retain(|artifact| match &for_platform {
+		Platform::All => true,
+		Platform::Host => {
+			if artifact.platform == "any" {
+				return true;
+			}
+
+			let os = if artifact.platform.contains("linux") {
+				host_targets
+					.iter()
+					.any(|t| t.operating_system() == Some(OS::Linux))
+			} else if artifact.platform.contains("windows") {
+				host_targets
+					.iter()
+					.any(|t| t.operating_system() == Some(OS::Win32))
+			} else if artifact.platform.contains("macos") {
+				host_targets
+					.iter()
+					.any(|t| t.operating_system() == Some(OS::Darwin))
+			} else {
+				false
+			};
+
+			let arch = if artifact.platform.contains("amd64") {
+				host_targets.iter().any(|t| t.arch_name() == "x86_64")
+			} else if artifact.platform.contains("arm64") {
+				host_targets.iter().any(|t| t.arch_name() == "aarch64")
+			} else if !artifact.platform.contains("-") {
+				true
+			} else {
+				false
+			};
+
+			os && arch
+		}
+		Platform::Match(s) => artifact.platform.contains(s),
+	});
 
 	artifacts.sort_by(|a, b| a.platform.cmp(&b.platform));
 	artifacts.sort_by(|a, b| a.artifact_type.cmp(&b.artifact_type));
+
+	Ok(artifacts)
+}
+
+pub async fn run(ctx: Context<TamanuArgs, ArtifactsArgs>) -> Result<()> {
+	let ArtifactsArgs { version, platform } = ctx.args_sub;
+	let artifacts = get_artifacts(&version, &platform).await?;
+
+	if artifacts.is_empty() {
+		println!(
+			"No artifacts found for version {} with the specified platform filter",
+			version
+		);
+		return Ok(());
+	}
 
 	let mut table = Table::new();
 	table
