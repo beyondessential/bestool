6fbd4b17b40c88af45b35860a2aae0cc22a2c819
Félix Saparelli <felix@bes.au>
2026-05-05 16:32:57 +1200

    feat(improv-wifi): protocol constants and enums

--- crates/improv-wifi/src/lib.rs
+++ crates/improv-wifi/src/lib.rs
@@ -6,3 +6,27 @@
 //!
 //! [Improv Wi-Fi]: https://www.improv-wifi.com
 #![cfg(target_os = "linux")]
+
+use bluer::Uuid;
+
+mod error;
+mod state;
+
+pub use error::Error;
+pub use state::{Capabilities, Status};
+
+pub const SERVICE_UUID: Uuid = Uuid::from_u128(0x00467768_6228_2272_4663_277478268000);
+pub const CHARACTERISTIC_UUID_CAPABILITIES: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268005);
+pub const CHARACTERISTIC_UUID_CURRENT_STATE: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268001);
+pub const CHARACTERISTIC_UUID_ERROR_STATE: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268002);
+pub const CHARACTERISTIC_UUID_RPC_COMMAND: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268003);
+pub const CHARACTERISTIC_UUID_RPC_RESULT: Uuid =
+	Uuid::from_u128(0x00467768_6228_2272_4663_277478268004);
+
+/// 16-bit Service Data UUID used in BLE advertisements (`0x4677`).
+pub const ADVERTISEMENT_SERVICE_DATA_UUID: Uuid =
+	Uuid::from_u128(0x00004677_0000_1000_8000_00805f9b34fb);
